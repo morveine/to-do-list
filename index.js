a4f92473329872e2c643b6462892b6dc92fdf473
morveine <morveine@gmail.com>
2023-07-05 15:01:57 +0200

    add more functionality

--- index.js
+++ index.js
@@ -6,16 +6,35 @@
 // separate 'done' folder
 
 const newTaskForm = document.getElementById('new-task-form');
+const newFolderForm = document.getElementById('new-folder-form')
 const taskTemplate = document.getElementById('task-template');
 const taskWrapper = document.getElementById('tasks-wrapper');
+const addTaskButton = document.getElementsByClassName('add-task')[0];
+const addFolderButton = document.getElementsByClassName('add-folder')[0];
+const formCancelButton = document.getElementById('cancel');
+
+addTaskButton.addEventListener('click', () => {
+    hideElement(addTaskButton);
+    showElement(newTaskForm);
+});
+
+formCancelButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    hideElement(newTaskForm);
+    showElement(addTaskButton);
+})
 
 newTaskForm.addEventListener('submit', makeNewTask);
+
+let n = 1;
 class Task {
     constructor(name, description, deadline, done = false) {
         this.name = name;
         this.description = description;
         this.deadline = deadline;
         this.done = done;
+        this.id = `task-${n}`
+        n++
     }
 }
 
@@ -53,18 +72,26 @@ function addTaskToDOM(task){  // TODO: change id of task when adding
     clone.querySelector('.dom-task-name').textContent = task.name;
 	clone.querySelector('.description').textContent = task.description;
 	clone.querySelector('.deadline').textContent = task.deadline;
-    clone.removeAttribute('id');
+    clone.id;
 }
 
-function deleteTask(){}
+// function deleteTask(){}
 
 function editTask(){} // should be able to change name, description, deadline, everything
 
-function makeFolder(){}
+function makeNewFolder(){}
 
-function addToFolder(){}
+function addToFolder(){} // all tasks created inside a folder belong to that folder
 
-function markTaskAsDone(){}
+function markTaskAsDone(){} // should change style of task and move it away
 
 function addFoldertoSidebar(){}
+
+function showElement(el){
+    el.classList.remove('hidden');
+}
+
+function hideElement(el){
+    el.classList.add('hidden');
+}
 
